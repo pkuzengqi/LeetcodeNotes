7e1df408e57230481e0835dc193acafd78281cd3
pkuzengqi <pkuzengqi@pku.edu.cn>
2019-03-02 21:08:06 -0500

    628

---
+++ 628. Maximum Product of Three Numbers.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max(nums[n - 1] * nums[n - 2] * nums[n - 3], nums[0] * nums[1] * nums[n - 1]);
+    }
+};
