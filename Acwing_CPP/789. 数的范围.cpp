d43e5a40d853297b6ccafc2de803b7cbc647af55
pkuzengqi <pkuzengqi@pku.edu.cn>
2019-07-09 23:56:23 +0800

    20190709

---
+++ Acwing_CPP/789. 数的范围.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+const int N=100010;
+
+int main(){
+    
+    int n,q;
+    int nums[N];
+    scanf("%d%d",&n,&q);
+    for(int i=0;i<n;++i) scanf("%d",&nums[i]);
+    
+    while(q--){
+        int k;
+        scanf("%d",&k);
+        
+        // 标准二分
+        int l=0, r=n-1;
+        while(l<r){
+            int mid = (l+1ll+r)/2;
+            if(nums[mid]>=k) r=mid;
+            else l=mid+1;
+        }
+        
+        
+        if(q[l]!=k) cout << "-1 -1"<< endl;
+        else{
+            cout <<l<< ' ';//退出循环时l是左边界,需要二分寻找右边界
+            r=n-1;
+            while(l<r){
+                int mid=(l+1ll+r)/2;
+                if(nums[mid]<=x) l=mid;
+                else r=mid-1;
+            }
+            cout<<l<<endl;
+        }
+    }
+    
+    
+    
+    return 0;
+}
