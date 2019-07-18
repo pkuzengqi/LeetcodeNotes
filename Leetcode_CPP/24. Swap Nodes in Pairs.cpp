daf785a8d8f4e0fc2156bf15f80dbd2a4c7e1bd4
Qi Zeng <pkuzengqi@pku.edu.cn>
2019-07-18 21:15:35 +0800

    20190718

--- Leetcode_CPP/24. Swap Nodes in Pairs.cpp
+++ Leetcode_CPP/24. Swap Nodes in Pairs.cpp
@@ -32,3 +32,30 @@ public:
         return dummy->next;
     }
 };
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        
+        auto p=dummy;
+        while(p && p->next && p->next->next){
+            auto q=p->next, k=q->next,l=k->next;
+            p->next=k, k->next=q, q->next=l, p=q;
+        }
+        
+        return dummy->next;
+    }
+};
