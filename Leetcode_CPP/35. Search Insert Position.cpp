fd9053f96a8f9bab427318fd502c6397c07df193
Qi Zeng <pkuzengqi@pku.edu.cn>
2019-07-20 18:11:42 +0800

    20190720

--- Leetcode_CPP/35. Search Insert Position.cpp
+++ Leetcode_CPP/35. Search Insert Position.cpp
@@ -12,3 +12,19 @@ public:
         return nums[l]<target? l+1:l; //l是最有可能在的位置，但不知左右
     }
 };
+
+
+//follow template
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(nums.empty() || nums.back()<target) return nums.size();
+        int l=0, r=nums.size()-1;
+        while(l<r){
+            int mid=(l+0ll+r)/2;
+            if(nums[mid]>=target) r=mid;
+            else l=mid+1;
+        }
+        return l;
+    }
+};
