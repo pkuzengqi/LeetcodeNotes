8c253c99870cc9dc8fdea01776e5d88419185058
Qi Zeng <pkuzengqi@pku.edu.cn>
2019-07-20 19:29:58 +0800

    20190720

--- Leetcode_CPP/33. Search in Rotated Sorted Array.cpp
+++ Leetcode_CPP/33. Search in Rotated Sorted Array.cpp
@@ -18,3 +18,38 @@ public:
       return nums[l]==target?l:-1;
     }
 };
+
+
+// 两次二分 应用模板
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.empty()) return -1;
+        int l = 0, r = nums.size() - 1;
+        
+        if(nums.back()<=nums[0]){
+            // first search to get break point l: same as leetcode 153
+            while (l < r)
+            {
+                int mid=(l+0ll+r)/2;
+                if(nums[mid]<nums[0]) r=mid;
+                else l=mid+1;
+            }
+            
+            //determine which part to search
+            if(target<nums[0]) r=nums.size()-1;
+            else r=l-1, l=0;
+        }
+        
+
+        
+        // second search to get target: similar to leetcode 74
+        while(l<r){
+            int mid = (l+1ll+r)/2;
+            if(nums[mid]<=target) l=mid;
+            else r=mid-1;
+        }
+        return target==nums[l]?l:-1;
+    }
+};
