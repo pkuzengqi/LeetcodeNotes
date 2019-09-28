d5a9d95a614ee52fda611c33abbb4edd183a6ef6
pkuzengqi <pkuzengqi@pku.edu.cn>
2019-09-28 07:49:25 -0500

    20190928

--- Leetcode_CPP/167. Two Sum II - Input array is sorted.cpp
+++ Leetcode_CPP/167. Two Sum II - Input array is sorted.cpp
@@ -1,3 +1,14 @@
+//最简单的双指针
+//167. Two Sum II - Input array is sorted
+//题目描述：给定一个按升序排序的数组，请找出两个数，使得它们的和等于 targettarget。
+//这道题的坑是返回值是index+1
+
+
+//(双指针扫描) O(n)O(n)
+// 用两个指针 i,ji,j 分别从数组首尾往中间扫描，
+// 每次将 ii 后移一位，然后不断前移 jj，
+// 直到 numbers[i]+numbers[j]≤target为止。
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
--- Leetcode_CPP/88. Merge Sorted Array.cpp
+++ Leetcode_CPP/88. Merge Sorted Array.cpp
@@ -1,3 +1,16 @@
+// ### 题目描述
+// 给定两个有序的整数数组nums1和nums2，要求将nums2合并进nums1中，并使nums1有序。
+
+
+//(线性合并) 时间复杂度 $O(m+n)$,  空间复杂度$O(1)$
+// 1. 设置`cur`指针指向合并后的`nums1`数组(大小为m+n)的最后一个元素，
+// `p`指向合并前的`nums1`数组(大小为m)的最后一个元素，
+// `q`指向`nums2`数组(大小为n)的最后一个元素。
+// 2. 比较`p`指向的值和`q`指向的值，将大的值挪进`nums1[cur]`。
+// 3. `cur`指针往前挪，`p`或者`q`指针也相应往前挪。
+// 4. 循环以上步骤直到p=0或q=0
+
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
